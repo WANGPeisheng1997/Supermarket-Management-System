060a693a584479f62eddc71e1e07ec9e91461b3b
王沛晟 <115681033@qq.com>
2017-04-17 15:29:48 +0800

    [wps] register
    try to use ‘insert’ in mysql

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -18,3 +18,17 @@ def check_if_account_exists(username,password):
         return True
     else:
         return False
+
+def register(username,password,email):
+    conn = pymysql.connect(host='10.221.137.167',
+                           user='root',
+                           db='supermarket',
+                           passwd='a887400',
+                           port=3306,
+                           charset="utf8"
+                           )
+
+    cursor = conn.cursor()
+    sql = "INSERT INTO memberacc VALUES(1,'%s','%','123213123','%s','2018-2-8',0,true,'')"
+    data = (username, password,email)
+    cursor.execute(sql % data)
