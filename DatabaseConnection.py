bd8f23614ead865ca5c05832478de8a9fa207a49
王沛晟 <115681033@qq.com>
2017-04-20 00:30:04 +0800

    [wps] add three commands
    1. exec_add_new_shopping_record
    2. exec_add_new_shopping_list
    3. exec_change_the_information_customor

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -17,7 +17,7 @@ class Connection(object):
         #                                   )
         self.connection = pymysql.connect(host=cloud_host,
                                           user='root',
-                                          db='test1',
+                                          db='test2',
                                           passwd='dbpj1008',
                                           port=4040,
                                           charset="utf8"
@@ -44,7 +44,7 @@ database = Connection()
 
 def exec_staff_login(username, password):
     database.connect_database()
-    sql = "SELECT * FROM staffacc WHERE empl_id='%s' AND staff_psw='%s'"
+    sql = "SELECT * FROM staffacc WHERE staff_id='%s' AND staff_psw='%s'"
     data = (username, password)
     database.exec_query(sql % data)
     values = database.fetch_cursor()
@@ -69,7 +69,7 @@ def exec_customer_login(username, password):
 
 def exec_supplier_login(username, password):
     database.connect_database()
-    sql = "SELECT supp_id FROM supplieracc WHERE supp_usname='%s' AND supp_pswd='%s'"
+    sql = "SELECT supp_id FROM supplieracc WHERE supp_name='%s' AND supp_psw='%s'"
     data = (username, password)
     database.exec_query(sql % data)
     values = database.fetch_cursor()
@@ -90,7 +90,7 @@ def exec_register_customer(username, password, email):
         return False
     else:
         time = str(datetime.now())  # insert a new customer
-        sql = "INSERT INTO memberacc(mem_name, mem_psw, mem_mail, mem_rtime, mem_point, mem_avai) VALUES('%s','%s','%s','%s',0,true)"
+        sql = "INSERT INTO memberacc(mem_name, mem_psw, mem_mail, mem_regtime, mem_point) VALUES('%s','%s','%s','%s',0)"
         data = (username, password, email, time)
         database.exec_update(sql % data)
         database.disconnect_database()
@@ -121,8 +121,15 @@ def exec_show_items():
 
 def exec_change_the_information_supplier(id, password, contact, phone, email, address, note):
     database.connect_database()
-    sql = "UPDATE supplieracc SET supp_pswd ='%s',supp_cname='%s',supp_mobinum='%s',supp_email='%s',supp_addr='%s',supp_note='%s' WHERE supp_id='%d'"
+    sql = "UPDATE supplieracc SET supp_psw ='%s',supp_contact='%s',supp_phone='%s',supp_mail='%s',supp_addr='%s',supp_note='%s' WHERE supp_id='%d'"
     data = (password, contact, phone, email, address, note, id)
+    database.exec_update(sql % data)
+    database.disconnect_database()
+
+def exec_change_the_information_customor(user_id, password, phone, email, note):
+    database.connect_database()
+    sql = "UPDATE memberacc SET mem_psw ='%s', mem_phone='%s', mem_mail='%s', mem_note='%s' WHERE mem_id='%d'"
+    data = (password, phone, email, note, user_id)
     database.exec_update(sql % data)
     database.disconnect_database()
 
@@ -132,4 +139,33 @@ def exec_add_new_complain_to_complain_list(mem_id, comp_type, comp_content):
     sql = "INSERT INTO complaint(mem_id, comp_type, comp_content, comp_time) VALUES('%d','%d','%s','%s')"
     data = (mem_id, comp_type, comp_content, time)
     database.exec_update(sql % data)
+    database.disconnect_database()
+
+def exec_add_new_shopping_record(mem_id):
+    database.connect_database()
+    time = str(datetime.now())
+    # insert a new shopping record
+    sql = "INSERT INTO shopping(mem_id, shop_time) VALUES('%d','%s')"
+    data = (mem_id,time)
+    database.exec_update(sql % data)
+    # return the shop_id
+    sql = "SELECT max(shop_id) FROM shopping"
+    database.exec_query(sql)
+    shop_id = database.fetch_cursor()
+    database.disconnect_database()
+    return shop_id
+
+def exec_add_new_shopping_list(shop_id, order_list):
+    database.connect_database()
+    time = str(datetime.now())
+    # insert a new shopping list
+    for current_item in order_list:
+        item_id = current_item[0]
+        item_quantity = current_item[1]
+        sql = "SELECT item_price FROM item WHERE item_id = '%d'"
+        database.exec_query(sql % item_id)
+        item_price = database.fetch_cursor()[0][0]
+        sql = "INSERT INTO shoplist VALUES('%d','%d','%.2f','%d')"
+        data = (item_id, shop_id, item_price, item_quantity)
+        database.exec_update(sql % data)
     database.disconnect_database()
--- transaction.py
+++ transaction.py
@@ -1,15 +1,45 @@
+# -*-coding:utf-8-*-
 import DatabaseConnection
 
 
-def process_purchase(order_list):
+def show_all_items_for_customer():
+    items = DatabaseConnection.exec_show_items()
+    return items
 
 
-# TODO DatabaseConnection.add_new_purchase_record(username, order_id)
-# TODO DatabaseConnection.change_the_credit_customer(username, )
-# TODO DatabaseConnection.change the_number_of_items_in_warehouse(order_list)
+def process_purchase(user_id, order_list):
+    pass
+    # TODO 没额外说明的就没有返回值
+    # TODO DatabaseConnection.add_new_purchase_record_to_table_purchlist(user_id, order_list)
+    # TODO DatabaseConnection.add_new_purchase_record_to_table_purchase(user_id, order_list)
+    # TODO DatabaseConnection.change_the_credit_customer_in_table_memberacc(user_id, order_list)
+    # TODO DatabaseConnection.change_the_number_of_items_in_table_shelf_list(order_list)
 
 
-def process_complain(user_id, complain_list):
-    comp_type = complain_list[0]
-    comp_content = complain_list[1]
-    DatabaseConnection.exec_add_new_complain_to_complain_list(user_id, comp_type, comp_content)
+def process_complain(user_id, complain_type, complain_content):
+    DatabaseConnection.exec_add_new_complain_to_complain_list(user_id, complain_type, complain_content)
+
+
+def process_edit_customer_info(user_id, telephone, email, notes):
+    pass
+    # TODO 注意用户自己不能编辑ID，用户名和用户密码，所以new_customer_info是不含user_id， 用户名，用户密码的列表
+    # TODO DatabaseConnection.change_user_information_in_table_memberacc(user_id, telephone, email, notes)
+
+
+def process_search_itmes_for_customer(search_string):
+    pass
+    # TODO 返回给我满足商品名称与search_string部分匹配的items的二维数组
+    # TODO DatabaseConnection.match_items = DatabaseConnection.search_matched_from_table_items(search_string)
+    # return match_items
+
+def show_all_information_of_customer(user_id):
+    pass
+    # TODO 返回编号为user_id的用户的所有信息，二维数组
+    # TODO user_info = DatabaseConnection.fetch_all_customer_information_from_table(user_id)
+    # return user_info
+
+def show_all_purchase_history_of_customer(user_id):
+    pass
+    # TODO 返回编号为user_id的用户的购买历史，格式为商品名，购买时间，单价格，(先不考虑商品数量的事情) 联合purchlist，item和purchase表搜索
+    # TODO history = fetch_all_records_from_table(user_id)
+    # TODO return history_
