5ca756d793aabe15b91361c08fa27219b68eca29
王沛晟 <115681033@qq.com>
2017-04-19 16:05:21 +0800

    [wps] add three functions
    1. exec_fetch_all_information_from_customer(user_id)
    2. exec_change_the_credit_customer(user_id, credits)
    3. exec_change_the_information_supplier(id, password, contact, phone,
    email, address, note)

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -57,28 +57,33 @@ def exec_staff_login(username, password):
 
 def exec_customer_login(username, password):
     database.connect_database()
-    sql = "SELECT * FROM memberacc WHERE mem_name='%s' AND mem_psw='%s'"
+    sql = "SELECT mem_id FROM memberacc WHERE mem_name='%s' AND mem_psw='%s'"
     data = (username, password)
     database.exec_query(sql % data)
     values = database.fetch_cursor()
+    database.disconnect_database()
     if values:
-        # find the customer's mem_id
-        sql = "SELECT mem_id FROM memberacc WHERE mem_name='%s'"
-        data = username
-        database.exec_query(sql % data)
-        mem_id = database.fetch_cursor()
-        database.disconnect_database()
-        return (True, mem_id)
+        return (True, values)
     else:
-        database.disconnect_database()
+        return (False, 0)
+
+def exec_supplier_login(username, password):
+    database.connect_database()
+    sql = "SELECT supp_id FROM supplieracc WHERE supp_usname='%s' AND supp_pswd='%s'"
+    data = (username, password)
+    database.exec_query(sql % data)
+    values = database.fetch_cursor()
+    database.disconnect_database()
+    if values:
+        return (True, values)
+    else:
         return (False, 0)
 
 def exec_register_customer(username, password, email):
     database.connect_database()
 
     sql = "SELECT * FROM memberacc WHERE mem_name='%s'"
-    data = username
-    database.exec_query(sql % data)
+    database.exec_query(sql % username)
     values = database.fetch_cursor()
     if values:  # if values is not empty, it indicates that username has been registered
         database.disconnect_database()
@@ -91,6 +96,21 @@ def exec_register_customer(username, password, email):
         database.disconnect_database()
         return True
 
+def exec_fetch_all_information_from_customer(user_id):
+    database.connect_database()
+    sql = "SELECT * FROM memberacc WHERE mem_id='%d'"
+    database.exec_query(sql % user_id)
+    values = database.fetch_cursor()
+    database.disconnect_database()
+    return values
+
+def exec_change_the_credit_customer(user_id, credits):
+    database.connect_database()
+    sql = "UPDATE memberacc SET mem_point = mem_point + '%d' WHERE mem_id='%d'"
+    data = (credits, user_id)
+    database.exec_update(sql % data)
+    database.disconnect_database()
+
 def exec_show_items():
     database.connect_database()
     sql = "SELECT * FROM item"
@@ -99,6 +119,12 @@ def exec_show_items():
     database.disconnect_database()
     return values
 
+def exec_change_the_information_supplier(id, password, contact, phone, email, address, note):
+    database.connect_database()
+    sql = "UPDATE supplieracc SET supp_pswd ='%s',supp_cname='%s',supp_mobinum='%s',supp_email='%s',supp_addr='%s',supp_note='%s' WHERE supp_id='%d'"
+    data = (password, contact, phone, email, address, note, id)
+    database.exec_update(sql % data)
+    database.disconnect_database()
 
 def exec_add_new_complain_to_complain_list(mem_id, comp_type, comp_content):
     database.connect_database()
